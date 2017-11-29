e7368aba6f8aa1e38c6f8ebcff30bdab47610ba7
Charles Peterson <cpeterson@brokerbin.com>
2017-11-29 13:47:13 -0600

    damn ubuntu

--- virtualbox-redis/redis-plus.sh
+++ virtualbox-redis/redis-plus.sh
@@ -15,8 +15,9 @@ cd /home/vagrant/.oh-my-zsh/custom/themes/powerlevel9k; git checkout color_names
 printf "\nsource ~/.bash_aliases\n" | tee -a /home/vagrant/.zshrc
 
 # https://stackoverflow.com/a/37331750/372215 --- allow for external use, the virtualbox can take care of "security" this is just for dev.
-sudo sed -i /etc/redis/redis.conf.original 's/^protected-mode.*/protected-mode no/' /etc/redis/redis.conf
-sudo sed -i '' 's/^bind/#bind/' /etc/redis/redis.conf
+sudo sed -i 's/^protected-mode.*/protected-mode no/' /etc/redis/redis.conf
+sudo sed -i 's/^bind/#bind/' /etc/redis/redis.conf
+
 # make sure redis will start
 sudo systemctl enable redis.service
 
